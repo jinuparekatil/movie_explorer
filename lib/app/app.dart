2abaa8c2ce214384f6e053ba96e84fc4e6b5c6cf
Jinu Joy <jinuparekatil@gmail.com>
2026-06-29 12:52:22 +0530

    Add application theme

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../features/home/presentation/pages/home_page.dart';
+import 'theme/app_theme.dart';
 
 class MovieExplorerApp extends StatelessWidget {
   const MovieExplorerApp({super.key});
@@ -9,6 +10,7 @@ class MovieExplorerApp extends StatelessWidget {
     return MaterialApp(
       title: 'Movie Explorer',
       debugShowCheckedModeBanner: false,
+      theme: AppTheme.light,
       home: HomePage(),
     );
   }
